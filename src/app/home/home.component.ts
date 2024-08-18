8f500fb69def0c3660870f2faff32e8d6e173cb3
Miguel Francisco de Freitas Fernandes <miguel.fernandes2004f2@gmail.com>
2024-08-17 21:56:04 -0300

    correção de cor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  isModalOpen = false;
+
+  openModal(): void {
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
+
   navigateTo(url: string): void {
     window.open(url, '_blank');
   }
